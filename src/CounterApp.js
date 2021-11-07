fcf0d76ec371db3929c8a12c67d51bc1d731af51
Edgard Vilo <evilo@hostyweb.cl>
2021-11-07 11:22:00 -0300

    clase 44

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,21 +1,26 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 5 }) => {
 
-    const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(value);
 
     //; handleAdd
     const handleAdd = (e) => {
         setCounter(counter + 1);
-        // setCounter( (c) => c + 1 );
     }
+
+    const handleSubstract = () =>  setCounter(counter - 1);
+
+    const handleReset = () => setCounter(value);
 
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{ counter }</h2>
 
+            <button onClick={ handleSubstract }>-1</button>
+            <button onClick={ handleReset }>Reset</button>
             <button onClick={ handleAdd }>+1</button>
         </>
     );
