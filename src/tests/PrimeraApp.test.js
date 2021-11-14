51a833075d0474874ccf0d4369730f4ec5e32ba1
Edgard Vilo <evilo@hostyweb.cl>
2021-11-14 18:33:17 -0300

    clase 59

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,12 +1,22 @@
 import { render } from '@testing-library/react';
+import { shallow } from 'enzyme/build';
 import PrimeraApp from '../PrimeraApp';
 
 describe('Pruebas en <PrimeraApp />', () => {
-    test('debe de mostrar el mensaje "Hola, soy Goku"', () => {
+    // test('debe de mostrar el mensaje "Hola, soy Goku"', () => {
        
-        const saludo = 'Hola, soy Goku';
-        const { getByText } = render(<PrimeraApp saludo={saludo} />);
+    //     const saludo = 'Hola, soy Goku';
+    //     const { getByText } = render(<PrimeraApp saludo={saludo} />);
         
-        expect(getByText(saludo)).toBeInTheDocument();
+    //     expect(getByText(saludo)).toBeInTheDocument();
+    // });
+    test('debe de mostrar <PrimeraApp /> correctamente', () => {
+      
+        const saludo = 'Hola, Soy Gokú';
+        const wrapper = shallow( <PrimeraApp saludo={saludo} /> );
+
+        expect(wrapper).toMatchSnapshot();
+
     })
-})
+    
+});
