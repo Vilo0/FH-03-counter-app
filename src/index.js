bb378277de6cca34e06725a1cead3e1aabba3d56
Edgard Vilo <evilo@hostyweb.cl>
2021-11-14 16:38:47 -0300

    clase 58

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import ReactDom from 'react-dom';
 
 import './index.css';
 import CounterApp from './CounterApp';
+import PrimeraApp from './PrimeraApp';
 
 const divRoot = document.querySelector('#root');
 
-ReactDom.render(<CounterApp value={ 30 } />, divRoot);
+// ReactDom.render(<CounterApp value={ 30 } />, divRoot);
+ReactDom.render(<PrimeraApp saludo="Hola, Soy Goku" value={ 30 } />, divRoot);
