f96e7263cb83d2626e86a7696b7c0d4a03ac501c
Edgard Vilo <evilo@hostyweb.cl>
2021-11-14 12:13:11 -0300

    clase 51

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,7 +1,12 @@
-test('debe ser true', () => {
-    const isActive = true;
-
-    if(isActive) {
-        throw new Error('No está activo');
-    }
+describe('pruebas en el archivo demo.test', () => {
+    test('deben de ser iguales los string', () => {
+        // inicialización
+        const mensaje = 'Hola Mundo';
+    
+        // estímulo
+        const mensaje2 = 'Hola Mundo';
+    
+        // observar el comportamiento
+        expect(mensaje).toBe(mensaje2);
+    });
 });
