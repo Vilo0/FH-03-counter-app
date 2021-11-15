daca02dcc05a271d80ab7860bf24c0e1c82905b9
Edgard Vilo <evilo@hostyweb.cl>
2021-11-14 22:24:02 -0300

    clase 62

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -1,13 +1,18 @@
-import '@testing-library/react';
+import '@testing-library/jest-dom';
 import { shallow } from 'enzyme/build';
 import CounterApp from '../CounterApp';
 
 describe('Pruebas en <CounterApp />', () => {
 
+    let wrapper = shallow( <CounterApp /> );
+
+    beforeEach(() => {
+        wrapper = shallow( <CounterApp /> );
+    });
+
     test('debe de mostrar <CounterApp /> correctamente y su valor por defecto', () => {
       
         const initial = 5;
-        const wrapper = shallow( <CounterApp /> );
 
         expect(wrapper).toMatchSnapshot();
 
@@ -22,6 +27,36 @@ describe('Pruebas en <CounterApp />', () => {
 
         const contador = wrapper.find('h2').text().trim();
         expect(contador).toBe('100');
+
+    });
+
+    test('debe de incrementar con el botón +1', () => {
+        
+        wrapper.find('button').at(2).simulate('click');
+
+        const contador = wrapper.find('h2').text().trim();
+
+        expect(contador).toBe('6');
+
+    });
+
+    test('debe de incrementar con el botón -1', () => {
+        
+        wrapper.find('button').at(0).simulate('click');
+
+        const contador = wrapper.find('h2').text().trim();
+
+        expect(contador).toBe('4');
+
+    });
+    
+    test('debe de incrementar con el botón reset', () => {
+        
+        wrapper.find('button').at(1).simulate('click');
+
+        const contador = wrapper.find('h2').text().trim();
+
+        expect(contador).toBe('5');
 
     });
 
